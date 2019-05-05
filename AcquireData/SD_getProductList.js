b5c943e005a11c0be0029e775c568aaa9a8941a1
PaurakhSharma <paurakh011@gmail.com>
2019-05-05 12:25:27 +0545

    Get details of all the products in the category 🎉

--- AcquireData/SD_getProductList.js
+++ AcquireData/SD_getProductList.js
@@ -5,9 +5,11 @@ module.exports = () => {
   console.log('hello')
   BASE_URL = 'https://www.sastodeal.com'
   CAT_URL = BASE_URL + '/sastodeal/cta-mobiles-28'
-  URL_WITH_RANGE = BASE_URL + '/sastodeal/cta-mobiles-28?flag=setFilters&toRange='
+  URL_WITH_RANGE =
+    BASE_URL + '/sastodeal/cta-mobiles-28?flag=setFilters&toRange='
 
   const productUrlList = []
+  const productList = []
   request(CAT_URL, (err, res, html) => {
     if (!err && res.statusCode === 200) {
       const $ = cheerio.load(html)
@@ -18,16 +20,47 @@ module.exports = () => {
       const totalProductCount = $('#countOfProduct').attr('value')
 
       request(URL_WITH_RANGE + totalProductCount, (err, res, html) => {
-        if(!err && res.statusCode === 200) {
+        if (!err && res.statusCode === 200) {
           const $ = cheerio.load(html)
 
-          $(".categoryProduct").each((i, product) => {
+          $('.categoryProduct').each((i, product) => {
             const $product = $(product)
             productURl = BASE_URL + $product.find('a').attr('href')
             productUrlList.push(productURl)
           })
         }
-        console.log(productUrlList)
+
+        productUrlList.forEach(productUrl => {
+          request(productUrl, (err, res, html) => {
+            if (!err && res.statusCode === 200) {
+              const $ = cheerio.load(html)
+
+              const name = $('.name').text().trim()
+              const details = $('.prodSpecification > ul').text().trim()
+              const imageUrl = $('.thumbslider > li:first-of-type > a').attr(
+                'data-image'
+              )
+              const originalPrice = parseInt(
+                $('.mrpPrice')
+                  .text()
+                  .replace('रू ', '')
+              )
+              const discountedPrice = parseInt(
+                $('.offerprize')
+                  .text()
+                  .replace('रू ', '')
+              )
+              productList.push({
+                name,
+                details,
+                imageUrl,
+                originalPrice,
+                discountedPrice
+              })
+              console.log(productList.length)
+            }
+          })
+        })
       })
     }
   })
