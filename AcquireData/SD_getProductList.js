7cb6d67c8649e6fc282f2dd1e78356a946abe6d2
PaurakhSharma <paurakh011@gmail.com>
2019-05-07 21:57:23 +0545

    Scrape all products from sastodeal 🚀

--- AcquireData/SD_getProductList.js
+++ AcquireData/SD_getProductList.js
@@ -2,14 +2,12 @@ const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 
 const productUrlList = []
-module.exports = async () => {
-  console.log('hello')
+module.exports = async (catUrl, category, subCategory, subSubCategory) => {
   BASE_URL = 'https://www.sastodeal.com'
-  CAT_URL = BASE_URL + '/sastodeal/cta-mobiles-28'
-  URL_WITH_RANGE =
-    BASE_URL + '/sastodeal/cta-mobiles-28?flag=setFilters&toRange='
+  CAT_URL = BASE_URL + catUrl
+  URL_WITH_RANGE = CAT_URL + '?flag=setFilters&toRange='
 
-  const response = await fetch(CAT_URL)
+  const response = await fetch(new URL(CAT_URL))
   const body = await response.text()
 
   const $ = cheerio.load(body)
@@ -18,9 +16,10 @@ module.exports = async () => {
     some of the items but we need to get all the products
     in the category. */
   const totalProductCount = $('#countOfProduct').attr('value')
+  console.log(totalProductCount)
 
   // hera all represent all the products in the category
-  const responseAll = await fetch(URL_WITH_RANGE + totalProductCount)
+  const responseAll = await fetch(new URL(URL_WITH_RANGE + totalProductCount))
   const bodyAll = await responseAll.text()
   const $all = cheerio.load(bodyAll)
 
@@ -32,17 +31,25 @@ module.exports = async () => {
   })
 
   const promises = []
-  productUrlList.forEach(productUrl => {
+  productUrlList.forEach((productUrl, index) => {
     // the response from `getProductDetail` is a promise
     // so store them in the promise array
-     promise = getProductDetail(productUrl)
+    promise = getProductDetail(productUrl, category, subCategory, subSubCategory)
     promises.push(promise)
   })
 
   // Now you get details of all the products in the category
-  Promise.all(promises).then((allProducts) => {
-    console.log(allProducts)
+  return Promise.all(promises).then((allProducts) => {
+    return allProducts
+  }).catch((error) => {
+    console.log('Error at promise.all', error)
   })
+}
+
+async function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
 /**
@@ -51,37 +58,53 @@ module.exports = async () => {
  * @param {string} url
  *
  */
-async function getProductDetail(url) {
-  const response = await fetch(url)
-  if (response.status === 200) {
-    const body = await response.text()
+async function getProductDetail(productUrl, category, subCategory, subSubCategory) {
+  try {
+    const response = await fetch(new URL(productUrl))
+    if (response.status === 200) {
+      const body = await response.text()
 
-    const $ = cheerio.load(body)
+      const $ = cheerio.load(body)
 
-    const name = $('.name')
-      .text()
-      .trim()
-    const details = $('.prodSpecification > ul')
-      .text()
-      .trim()
-    const imageUrl = $('.thumbslider > li:first-of-type > a').attr('data-image')
-    const originalPrice = parseInt(
-      $('.mrpPrice')
+      const name = $('.name')
         .text()
-        .replace('रू ', '')
-    )
-    const discountedPrice = parseInt(
-      $('.offerprize')
+        .trim()
+      const details = $('.prodSpecification > ul')
         .text()
-        .replace('रू ', '')
-    )
-
-    return {
-      name,
-      details,
-      imageUrl,
-      originalPrice,
-      discountedPrice
+        .trim()
+      const imageUrl = $('.thumbslider > li:first-of-type > a').attr('data-image')
+      const originalPrice = parseInt(
+        $('.mrpPrice')
+          .text()
+          .replace('रू ', '')
+      )
+      const discountedPrice = parseInt(
+        $('.offerprize')
+          .text()
+          .replace('रू ', '')
+      )
+        // console.log({
+        //   name,
+        //   details,
+        //   imageUrl,
+        //   originalPrice,
+        //   discountedPrice
+        // })
+        if (isNaN(originalPrice)) {
+          console.log("\x1b[31m", `Error in ${productUrl}`)
+        } 
+      return {
+        name,
+        details,
+        imageUrl,
+        originalPrice,
+        discountedPrice,
+        category,
+        subCategory,
+        subSubCategory
+      }
     }
+  } catch (error) {
+    console.log('Error founr', error)
   }
 }
