ecf9ca4a47865e1135a4ba514d2dfadf5c6b14a2
PaurakhSharma <paurakh011@gmail.com>
2019-07-25 23:55:50 +0545

    Refactor code for scrape SD_products

--- AcquireData/app.js
+++ AcquireData/app.js
@@ -2,11 +2,13 @@ const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
 const getSD_categories = require('./sastoDeal_categoryList')
+const getSD_products = require('./SD_getProductList')
 
 // Connection URL
 const url = 'mongodb://localhost:27017'
 const dbName = 'sastoBeforeDB'
 const SD_categories_col = 'SD_cat_col'
+const SD_products_col = 'SD_products_col'
 
 async function getClient() {
   // Create a new MongoClient
@@ -21,6 +23,14 @@ module.exports.SD_categories =  async () => {
   const client = await getClient()
 
   await getSD_categories(client, dbName, SD_categories_col)
+
+  await client.close()
+}
+
+module.exports.SD_products = async () => {
+  const client = await getClient()
+
+  await getSD_products(client, dbName, SD_categories_col, SD_products_col)
 
   await client.close()
 }
