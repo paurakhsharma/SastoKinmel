60ba2759c7d797ad0d794b4c40d0203c5ffd4d17
PaurakhSharma <paurakh011@gmail.com>
2019-07-28 22:13:43 +0545

    Add daraz categories scraper

--- AcquireData/app.js
+++ AcquireData/app.js
@@ -4,11 +4,15 @@ const assert = require('assert')
 const getSD_categories = require('./sastoDeal_categoryList')
 const getSD_products = require('./SD_getProductList')
 
+const getDaraz_categories = require('./Daraz_categoryList')
+
 // Connection URL
 const url = 'mongodb://localhost:27017'
 const dbName = 'sastoBeforeDB'
 const SD_categories_col = 'SD_cat_col'
 const SD_products_col = 'SD_products_col'
+
+const Daraz_categories_col = 'Daraz_cat_col'
 
 async function getClient() {
   // Create a new MongoClient
@@ -31,6 +35,14 @@ module.exports.SD_products = async () => {
   const client = await getClient()
 
   await getSD_products(client, dbName, SD_categories_col, SD_products_col)
+
+  await client.close()
+}
+
+module.exports.Daraz_categories = async () => {
+  const client = await getClient()
+
+  await getDaraz_categories(client, dbName, Daraz_categories_col)
 
   await client.close()
 }
