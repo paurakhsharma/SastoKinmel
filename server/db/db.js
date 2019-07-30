eb8489aedc0cb8a036acc809efb59670af12eac5
PaurakhSharma <paurakh011@gmail.com>
2019-07-30 16:05:31 +0545

    Add searchScore to make search better

--- server/db/db.js
+++ server/db/db.js
@@ -6,43 +6,53 @@ const url = 'mongodb://localhost/sastoBeforeDB'
 // Create a new MongoClient
 
 exports.GetAllProducts = function(page, limit, callback) {
-  mongoose.connect(url, {useNewUrlParser: true});
+  mongoose.connect(url, { useNewUrlParser: true })
   const db = mongoose.connection
-  db.on('error', console.error.bind(console, 'connection error:'));
+  db.on('error', console.error.bind(console, 'connection error:'))
 
   db.once('open', function() {
-    db.db.createIndex( { name: "text", detail: "text" } )
+    db.db.createIndex({ name: 'text', detail: 'text' })
     offset = limit * (page - 1)
-    db.db.collection('SD_products', function (err, collection) {
+    db.db.collection('SD_products', function(err, collection) {
       if (err) {
         console.error('There was an error', err)
       }
-      collection.find({
-        category: 'Women'
-      })
+      collection
+        .find({
+          category: 'Women'
+        })
         .limit(limit)
         .skip(offset)
-        .toArray(function (err, data) {
+        .toArray(function(err, data) {
           mongoose.disconnect()
           callback(data)
         })
     })
-  });
+  })
 }
 
 exports.Search = function(search_param, callback) {
-  mongoose.connect(url, {useNewUrlParser: true})
+  mongoose.connect(url, { useNewUrlParser: true })
   const db = mongoose.connection
   db.on('err', console.error.bind(console, 'connection error:'))
 
-  db.once('open', function () {
-    db.db.collection('Daraz_products_col', function (err, collection) {
+  db.once('open', function() {
+    db.db.collection('Daraz_products_col', function(err, collection) {
       if (err) {
         console.error('There was an error', err)
       }
       console.log(search_param)
-      collection.find({ $text: {$search: search_param}})
-        .toArray(function (err, data) {
+      collection
+        .find(
+          { $text: { $search: search_param } },
+          {
+            projection: { score: { $meta: 'textScore' } },
+            sort: { score: { $meta: 'textScore' } }
+          }
+        )
+        .toArray(function(err, data) {
+          console.log(err)
+          console.log(data)
           mongoose.disconnect()
           callback(data)
         })
