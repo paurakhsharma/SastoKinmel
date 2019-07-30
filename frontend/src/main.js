c41e72744a22c7effca7ba25d4e470bf5d3a463e
PaurakhSharma <paurakh011@gmail.com>
2019-07-30 16:06:52 +0545

    Remove fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,14 +6,8 @@ import './registerServiceWorker'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUserSecret)
 Vue.config.productionTip = false
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueAxios, axios)
 
 new Vue({
